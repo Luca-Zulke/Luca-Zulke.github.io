b814ee86e49ff63e88bb31574577623cc83d6b29
Luca <lucazulke@hotmail.com>
2022-09-02 18:51:50 +0200

    Updates

--- components/About.jsx
+++ components/About.jsx
@@ -5,21 +5,21 @@ import styles from '../styles/About.module.css';
 const About = () => {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.subContainer}>
-        <div className={styles.image}>
-          <Image className={styles.image1} src='/img/gallery3.jpg' alt=''  width='577' height='433'/>
-        </div>
-        <div className={styles.aboutContainer}>
-          <div className={styles.aboutItem}>OUR BISTRO</div>
-          <div className={styles.aboutItem}></div>
-          <div className={styles.aboutItem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</div>
-          <div className={styles.aboutItem}>Awarded Chefs
-          </div>
-          <div className={styles.aboutItem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.
+      
+      <div className={styles.imageContainer}>
+        <div className={styles.image}></div>
+      </div>
 
-          </div>
+      <div className={styles.aboutContainer}>
+        <div className={styles.aboutItem}>OUR BISTRO</div>
+        <div className={styles.aboutItem}></div>
+        <div className={styles.aboutItem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare diam commodo nibh.</div>
+        <div className={styles.aboutItem}>Lorem ipsum
         </div>
-      </div> */}
+        <div className={styles.aboutItem}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam. Sed commodo nibh ante facilisis bibendum dolor feugiat at. Duis sed dapibus leo nec ornare.
+        </div>
+      </div>
+
     </div>
   )
 }
