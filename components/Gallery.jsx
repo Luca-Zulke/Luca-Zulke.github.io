9f51b8d408b65ff133d8637334d198321a6b9ac8
Luca <lucazulke@hotmail.com>
2022-09-16 20:17:33 +0200

    Gallery

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,9 +1,55 @@
+import Image from 'next/image';
 import React from 'react'
 import styles from '../styles/Gallery.module.css';
 
 const Gallery = () => {
+  const images = [
+    "/img/gallery1.jpg",
+    "/img/gallery2.jpg",
+    "/img/gallery3.jpg",
+    "/img/gallery4.jpg",
+    "/img/gallery5.jpg",
+    "/img/gallery1.jpg",
+    "/img/gallery2.jpg",
+    "/img/gallery3.jpg",
+    "/img/gallery4.jpg",
+    "/img/gallery5.jpg",
+    "/img/gallery1.jpg",
+    "/img/gallery2.jpg",
+  ]
+
   return (
-    <div id='gallery'>Gallery</div>
+    <div className={styles.container}>
+      <div className={styles.categoryContainer}>
+        <div className={styles.categories}>
+          <ul>
+            <li><a href="#" data-filter="*" className={styles.active}>All</a></li>
+            <li><a href="#" data-filter="cat1">Category</a></li>
+            <li><a href="#" data-filter="cat2">Category</a></li>
+            <li><a href="#" data-filter="cat3">Category</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={styles.galleryContainer}>
+        <div className={styles.gallery}>
+          <div className={styles.galleryItems}>
+            {images.map((img, i) => (
+              <div key={i} className={styles.item}>
+                <div className={styles.itemHoverBg}>
+                  <a href={img} key={i} alt="">
+                    <div className={styles.hoverText}>
+                      {img.substring(img.lastIndexOf('/') + 1, img.indexOf('.')).split('_').join(' ')}
+                    </div>
+                    <Image className={styles.itemImg} src={img} key={i} layout="fixed" width={360} height={240} alt="" objectFit="cover"/>
+                  </a>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- components/Landing.jsx
+++ components/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
         {/* <Image src="/img/gallery1.jpg" alt="" layout="fill" width="1440" height='981' objectFit='cover' objectPosition='top' quality='100'/> */}
          {/* Center */} 
          <div className={styles.centerContainer}>
-          <div className={styles.logo}>De Lichtaarste Kwezel</div>
+          <div className={styles.logo}>De Lichtaartse Kwezel</div>
           <div className={styles.description}>Bistro / Coffee / Bar</div>
           <button className={styles.button}>DISCOVER STORY</button>
         </div>
