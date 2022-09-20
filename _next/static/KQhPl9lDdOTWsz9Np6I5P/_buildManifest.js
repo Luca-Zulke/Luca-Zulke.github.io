b0a3c91d164619bca1023885e491fee9dfa7d856
Luca-Zulke <63809233+Luca-Zulke@users.noreply.github.com>
2022-09-20 17:55:28 +0000

    Deploying to gh-pages from @ Luca-Zulke/Luca-Zulke.github.io@a41a64b745b6db7b813d5104a529624568324c20 🚀

---
+++ _next/static/KQhPl9lDdOTWsz9Np6I5P/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/089a5394997acdce.css","static/chunks/pages/index-f95290339b05d1db.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- _next/static/i4XcfkSAKV4rCRO9IJfJG/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f0d04c582c35a045.css","static/chunks/pages/index-953aea1f8d0428f7.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
