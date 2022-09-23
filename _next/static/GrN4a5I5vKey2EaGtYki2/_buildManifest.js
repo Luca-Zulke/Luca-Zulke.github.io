00d938a36e185f734c1d916cdd88e8aef6ce626f
Luca-Zulke <63809233+Luca-Zulke@users.noreply.github.com>
2022-09-23 16:21:46 +0000

    Deploying to gh-pages from @ Luca-Zulke/Luca-Zulke.github.io@88a8a12c563d49f4600d3cc89546dde1dba58f57 🚀

---
+++ _next/static/GrN4a5I5vKey2EaGtYki2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/00e9ad336483965f.css","static/chunks/pages/index-8d6987b528939ca5.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
